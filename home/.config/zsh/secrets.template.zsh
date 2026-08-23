11452909e754b5a88360cdea48e5df8025244fa2
utopy <30875147+utopyin@users.noreply.github.com>
2026-08-23 12:21:01 +0200

    add axiom and firecrawl tokens

--- home/.config/zsh/secrets.template.zsh
+++ home/.config/zsh/secrets.template.zsh
@@ -5,7 +5,6 @@
 #   ~/.dotfiles/dot secrets render
 #
 # Expected 1Password locations. These are Password-category items in the Personal vault.
-
 export GITHUB_TOKEN="op://Personal/Dotfiles GitHub Token/password"
 export GEMINI_API_KEY="op://Personal/Dotfiles Gemini API Key/password"
 export CEREBRAS_API_KEY="op://Personal/Dotfiles Cerebras API Key/password"
@@ -15,3 +14,5 @@ export UIDOTSH_BEARER="Bearer ${UIDOTSH_TOKEN}"
 export EXCALIDRAW_API_KEY="op://Personal/Dotfiles Excalidraw API Key/password"
 export GH_REGISTRY_TOKEN="op://Personal/Dotfiles GH Registry Token/password"
 export FIREWORKS_API_KEY="op://Personal/Dotfiles Fireworks API Token/password"
+export AXIOM_QUERY_TOKEN="op://Personal/Dotfiles AXIOM_QUERY_TOKEN/password"
+export FIRECRAWL_API_KEY="op://Personal/Dotfiles FIRECRAWL_API_KEY/password"

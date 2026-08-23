9ee5b03c6ebbd7466dca752aa97ec69938af84d5
utopy <30875147+utopyin@users.noreply.github.com>
2026-08-23 15:57:19 +0200

    remove cloudfalre api token

--- home/.config/zsh/secrets.example.zsh
+++ home/.config/zsh/secrets.example.zsh
@@ -4,7 +4,6 @@
 # export GITHUB_TOKEN="op://Personal/Dotfiles GitHub Token/password"
 # export GEMINI_API_KEY="op://Personal/Dotfiles Gemini API Key/password"
 # export CEREBRAS_API_KEY="op://Personal/Dotfiles Cerebras API Key/password"
-# export CLOUDFLARE_API_TOKEN="op://Personal/Dotfiles Cloudflare API Token/password"
 # export UIDOTSH_TOKEN="op://Personal/Dotfiles UIDOTSH Token/password"
 # export UIDOTSH_BEARER="Bearer ${UIDOTSH_TOKEN}"
 # export EXCALIDRAW_API_KEY="op://Personal/Dotfiles Excalidraw API Key/password"

eaac8213ea58193386e1c42eb37a422d165c86a1
utopy <30875147+utopyin@users.noreply.github.com>
2026-08-25 17:04:12 +0200

    add(hypr/slack): bind Slack to Hyper+S

--- home/linux/.config/hypr/bindings.lua
+++ home/linux/.config/hypr/bindings.lua
@@ -19,6 +19,7 @@ o.bind("MOD3 + C", "Zed", { launch = "zeditor" })
 
 -- Show or hide maximized app scratchpads.
 o.bind("MOD3 + L", "Toggle Linear", "~/.local/bin/toggle-linear")
+o.bind("MOD3 + S", "Toggle Slack", "~/.local/bin/toggle-slack")
 o.bind("MOD3 + B", "Toggle browser", "~/.local/bin/toggle-browser")
 
 hl.unbind("ALT + CTRL + J")
